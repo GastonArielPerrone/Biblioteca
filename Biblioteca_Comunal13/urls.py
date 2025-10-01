3a0436476ac82f5bdfc9c7e53c64f6b9db8fa366
Gastón Ariel Perrone <Perrone.gaston.ariel@outlook.com>
2025-10-01 11:19:25 -0300

    End point about index.html is "".

--- Biblioteca_Comunal13/urls.py
+++ Biblioteca_Comunal13/urls.py
@@ -23,7 +23,7 @@ from apps.empleados.views import index, register
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     # API V1
-    path('index/', index, name='index'),
+    path('', index, name='index'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
