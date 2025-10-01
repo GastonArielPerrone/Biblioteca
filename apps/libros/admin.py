5eeaecbd3f4254966de287213766c3919671d1fc
Gastón Ariel Perrone <Perrone.gaston.ariel@outlook.com>
2025-10-01 17:57:12 -0300

    changes are applyed in admin.py about libros.

--- apps/libros/admin.py
+++ apps/libros/admin.py
@@ -5,7 +5,7 @@ from apps.libros.models import Libro
 # Register your models here.
 @admin.register(Libro)
 class LibroAdmin(admin.ModelAdmin):
-    list_display = ("titulo",
+    list_display = ("titulo_libro",
                     "autor",
                     "editorial",
                     "categoria",
@@ -13,10 +13,10 @@ class LibroAdmin(admin.ModelAdmin):
                     "cantidad",
                     "creado",
                     "actualizado",)
-    search_fields = ("titulo",
-                    "autor__nombre",
-                    "editorial__nombre",
-                    "categoria__nombre",
+    search_fields = ("titulo_libro",
+                    "nombre_autor",
+                    "nombre_editorial",
+                    "nombre_categoria",
                     "fecha_publicacion",
                     "cantidad",
                     "creado",
